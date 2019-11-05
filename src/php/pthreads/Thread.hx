918fcb57fe3029654c288a08e00889a65d155ad5
JackNoordhuis <jackjnoordhuis@gmail.com>
2019-11-05 17:51:19 +1100

    Remove visibility modifiers from thread function definitions

--- src/php/pthreads/Thread.hx
+++ src/php/pthreads/Thread.hx
@@ -18,53 +18,53 @@ extern class Thread extends Threaded{
 	 *
 	 * @link http://www.php.net/manual/en/thread.getcreatorid.php
 	 */
-    public function getCreatorId():Int;
+    function getCreatorId():Int;
 
     /**
 	 * Will return the instance of currently executing thread
 	 */
-    public static function getCurrentThread():Thread;
+    static function getCurrentThread():Thread;
 
     /**
 	 * Will return the identity of the currently executing thread
 	 *
 	 * @link http://www.php.net/manual/en/thread.getcurrentthreadid.php
 	 */
-    public static function getCurrentThreadId():Int;
+    static function getCurrentThreadId():Int;
 
     /**
 	 * Will return the identity of the referenced Thread
 	 *
 	 * @link http://www.php.net/manual/en/thread.getthreadid.php
 	 */
-    public function getThreadId():Int;
+    function getThreadId():Int;
 
     /**
 	 * Tell if the referenced Thread has been joined by another context
 	 *
 	 * @link http://www.php.net/manual/en/thread.isjoined.php
 	 */
-    public function isJoined():Bool;
+    function isJoined():Bool;
 
     /**
 	 * Tell if the referenced Thread has been started
 	 *
 	 * @link http://www.php.net/manual/en/thread.isstarted.php
 	 */
-    public function isStarted():Bool;
+    function isStarted():Bool;
 
     /**
 	 * Causes the calling context to wait for the referenced Thread to finish executing
 	 *
 	 * @link http://www.php.net/manual/en/thread.join.php
 	 */
-    public function join():Bool;
+    function join():Bool;
 
     /**
 	 * Will start a new Thread to execute the implemented run method
 	 *
 	 * @link http://www.php.net/manual/en/thread.start.php
 	 */
-    public function start(options:Int = 0x111111):Bool;
+    function start(options:Int = 0x111111):Bool;
 
 }
